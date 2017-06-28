faab99ce510e4a5cb6dedd7a348373f101fe10fc
zhangximufeng <357321515@qq.com>
2017-06-28 16:37:11 +0800

    upd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
  var path = require('path');
- 
+console.log("沐枫目前单身再找女朋友~~O(∩_∩)O哈哈~")
+console.log("微信:zhangximufeng") 
  var arguments = process.argv.splice(2);
 
 console.log("arguments:", arguments);
